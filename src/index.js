d3d2d8679271cbce4767a4111b9f877bf0b9fb9c
Namrata-Raina <nr222@snu.edu.in>
2021-05-21 01:53:40 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+import { Provider } from 'react-redux';
+import store from './store';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6HnQbyiBU1TOL_xri_z_Acw08ouVuTpQ",
+  authDomain: "web-messenger-65008.firebaseapp.com",
+  databaseURL: "https://web-messenger-65008.firebaseio.com",
+  projectId: "web-messenger-65008",
+  storageBucket: "web-messenger-65008.appspot.com",
+  messagingSenderId: "126842583302",
+  appId: "1:126842583302:web:82fd9141c4faca47fc4687",
+  measurementId: "G-7CLL5F60JN"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+window.store = store;
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
